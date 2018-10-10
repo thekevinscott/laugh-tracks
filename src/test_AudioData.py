461cee5e4f9a375a0cd9c11e751de762353fa505
Kevin Scott <kevin@hitheory.com>
2018-10-10 09:51:55 -0400

    add pytest watch

--- src/test_AudioData.py
+++ src/test_AudioData.py
@@ -4,4 +4,4 @@ import numpy as np
 def test_it_handles_already_normalized_data():
     audioData = AudioData()
     arr = np.array([0, 1])
-    assert audioData.normalize(arr) == arr
+    assert (audioData.normalize(arr) == arr).all()
